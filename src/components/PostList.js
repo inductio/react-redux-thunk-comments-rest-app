6229bed127cf5285f649aed7c345156871299ff1
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-07 16:02:17 +0200

    users fetch data by id implemented

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,20 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
 
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user blue" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                        <div>
+                            <UserHeader userId={post.userId}/>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
-        console.log(this.props);
-        return <div>Post List</div>
+        return <div className="ui relaxed divided list">
+            {this.renderList()}
+        </div>
     }
 }
 
 const mapStateToProps = (state) => {
-    return {posts: state.posts}
+    return {
+        posts: state.posts[0] || []
+    }
 };
 
 export default connect(mapStateToProps, {
