f7dfc6489738307cf15965931b6d28773d71753b
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-10 13:39:42 +0200

    Fetch post comments by click implemented

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -4,15 +4,12 @@ import { fetchComments } from '../actions';
 import CommentsList from "./CommentsList";
 
 class PostComments extends Component {
-    componentDidMount() {
-        this.props.fetchComments(this.props.postId);
-    }
-
     render() {
         const { comments } = this.props;
 
         if (!comments.length) {
-            return <div>Comments Loading...</div>;
+            return <button onClick={() => this.props.fetchComments(this.props.postId)}
+                className="ui teal basic button">Show comments</button>;
         }
 
         return <CommentsList comments={comments}/>
